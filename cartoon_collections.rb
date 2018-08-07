a4c0137f564c8893a37addfa0866a8bd873a48d8
Nasser Sanou <nassersanou23@gmail.com>
2018-08-07 22:22:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -40,9 +40,9 @@ def find_the_cheese(cheese_types)
     
   if cheese_types.include?(cheese)
      return "#{cheese}"
-    # else 
-    # return nil
+    else 
+    return nil
    end
-end
+  end
 end
  find_the_cheese(cheese_types)
