3f43f1799560b8a41872885bf40456ea9d19c4fc
Nasser Sanou <nassersanou23@gmail.com>
2018-08-07 22:04:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,8 +37,12 @@ long_planeteer_calls(planeteer_calls)
 cheese_types = ["cheddar", "gouda", "camembert"]
 def find_the_cheese(cheese_types)
 
-    cheese_types.find do |cheese|
-    cheese_types.include?(cheese)
-  end
+    cheese_types.find do |cheese| 
+    if cheese_types.include?(cheese)
+     return true
+    else 
+    return "nil"
+   end
+  end 
 end
  find_the_cheese(cheese_types)
